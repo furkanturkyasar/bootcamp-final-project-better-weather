59088927b4c03c5ff5bc83f71ca2cc9b6c5de610
Furkan Turkyasar <turkyasarfurkan@gmail.com>
2022-09-10 19:27:34 +0300

    fetching weather data from past searches added

--- src/components/MinifiedCard/MinifiedCard.js
+++ src/components/MinifiedCard/MinifiedCard.js
@@ -1,14 +1,30 @@
 import withStates from "../../hocs/withStates";
+import { WEATHER_API_URL, API_KEY } from "../../services/api";
 
-const MinifiedCard = ({ cities }) => {
+const MinifiedCard = ({ cities, setWeather, setCities }) => {
   const slicedCities = cities.slice(Math.max(cities.length - 3, 0)).reverse();
+
+  const fetchCityWeather = (item) => {
+    fetch(`${WEATHER_API_URL}/weather?q=${item}&appid=${API_KEY}&units=metric`)
+      .then((response) => response.json())
+      .then((response) => {
+        setWeather(response);
+        localStorage.setItem("weather", JSON.stringify(response));
+        setCities([...cities, response.name]);
+        localStorage.setItem(
+          "city",
+          JSON.stringify([...cities, response.name])
+        );
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
     <div className="minified-card">
       {slicedCities.map((item, index) => (
         <div className="card-1 card" key={index}>
           <ul>
-            <li>{item}</li>
+            <li onClick={() => fetchCityWeather(item)}>{item}</li>
           </ul>
         </div>
       ))}
